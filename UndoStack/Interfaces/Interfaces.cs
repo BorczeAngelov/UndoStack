b59f40fa41aa3c0c45b78ac2c2ced9293a66837c
Borcze Angelov <borce.angelov@yahoo.com>
2019-05-11 16:52:08 +0200

    add basic interfaces

---
+++ UndoStack/Interfaces/Interfaces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndoStack.Interfaces
+{
+    public interface ITwoWayAction
+    {
+        void ExecuteOrRedo();
+        void RevertOrUndo();
+        void MergeWithNextIfNeeded(ITwoWayAction nextTwoWayAction);
+    }
+
+    public interface IUndoStackManager
+    {
+        void AddAndExecute(ITwoWayAction twoWayAction);
+        void Redo();
+        void Undo();
+        void Clear();
+    }
+}
